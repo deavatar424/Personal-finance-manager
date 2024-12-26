d293bedb37d0e38cd3180c7f59041e573f8a7b21
deavatar <deavatar4@gmail.com>
2024-12-26 15:13:26 +0100

    cors installation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const budgetRoutes = require('./routes/budgetRoutes');
@@ -8,14 +9,19 @@ const transactionRoutes = require('./routes/transactionRoutes');
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors({
+    credentials:true
+}));
 
-mongoose.connect(process.env.DB_URI,);
+
 
 app.use('/api', authRoutes);
 app.use('/api/budgets', budgetRoutes);
 app.use('/api/transactions', transactionRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,async() => {
+    mongoose.connect(process.env.DB_URI,);
+    console.log("database connected")
     console.log(`Server running on port ${PORT}`);
 });
