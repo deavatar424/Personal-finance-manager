9c6b9254a3cf18cb297caa47c1d5e2548d9d85cd
deavatar <deavatar4@gmail.com>
2024-12-27 20:19:19 +0100

    cors installation

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { addTransaction, getTransactions, deleteTransactions } = require('../controllers/transactionController');
+const { addTransaction, getTransactions, deleteTransactions, updateTransactions } = require('../controllers/transactionController');
 const { authenticateJWT } = require('../middleware/authmiddleware');
 const router = express.Router();
 
 router.use(authenticateJWT);
 router.post('/', addTransaction);
 router.get('/', getTransactions);
+router.put('/:id', updateTransactions);
 router.delete('/:id', deleteTransactions);
 
 // Add other transaction routes similarly
